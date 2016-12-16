ca5b2ca6c76daeea9f141d16eb6c33f68ee7211e
Michael Lazar <lazar.michael22@gmail.com>
2016-12-16 13:42:52 -0800

    Adding Heroku scaffolding.

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +8,5 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
