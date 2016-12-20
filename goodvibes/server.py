32e04d7704ea682bb283c403b39d4a72b2a9a5c8
Michael Lazar <lazar.michael22@gmail.com>
2016-12-19 18:53:33 -0800

    Fixed a bug in the search box.

--- goodvibes/server.py
+++ goodvibes/server.py
@@ -3,30 +3,34 @@ import random
 
 from flask import Flask, render_template, url_for, redirect, abort
 
-FLASH_DIR = os.path.join(os.path.dirname(__file__), 'static', 'flash')
-
 app = Flask(__name__)
 
+def _get_flashes():
+    # Refresh on every request to support adding files to the directory
+    flash_dir = os.path.join(os.path.dirname(__file__), 'static', 'flash')
+    flashes = (f for f in os.listdir(flash_dir) if f.endswith('.swf'))
+    return sorted(f[:-4] for f in flashes)
 
 @app.route("/")
 def get_index():
-    flashes = sorted(f for f in os.listdir(FLASH_DIR) if f.endswith('.swf'))
+    flashes = _get_flashes()
     flash = random.choice(flashes)
     return redirect(url_for('get_flash', flash=flash))
 
 @app.route("/<flash>")
 def get_flash(flash=None):
-    flashes = sorted(f for f in os.listdir(FLASH_DIR) if f.endswith('.swf'))
+    flashes = _get_flashes()
     if flash not in flashes:
         abort(404)
-    
+
     index = flashes.index(flash)
+    source = os.path.join('flash', flash + '.swf')
     next_flash = flashes[(index + 1) % len(flashes)]
     prev_flash = flashes[(index - 1) % len(flashes)]
     return render_template(
         'index.html',
         flashes=flashes,
-        flash_source=url_for('static', filename=os.path.join('flash', flash)),
+        flash_source=url_for('static', filename=source),
         next_flash=url_for('get_flash', flash=next_flash),
         prev_flash=url_for('get_flash', flash=prev_flash))
 
